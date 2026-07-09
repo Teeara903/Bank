6d0b96b02d5e934591285693b909c211c7c49f86
Teeara903 <omobolanlegbelee@gmail.com>
2026-07-09 22:46:36 +0100

    changes made to the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Transfer from "./pages/Transfer";
@@ -17,6 +18,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route
@@ -27,61 +29,94 @@ function App() {
           </ProtectedRoute>
         }
       />
-      <Route path="/transfer" element={
-        <ProtectedRoute>
-          <Transfer />
-        </ProtectedRoute>
-      } />
-      <Route path="/transactions" element={
-        <ProtectedRoute>
-          <Transactions />
-        </ProtectedRoute>
-      } />
-      <Route path="/forgot-password" element={
-        <ProtectedRoute>
-          <ForgotPassword />
-        </ProtectedRoute>
-      } />
-      <Route path="/profile" element={
-        <ProtectedRoute>
-          <Profile />
-        </ProtectedRoute>
-      } />
-      <Route path="/change-password" element={
-        <ProtectedRoute>
-          <ChangePassword />
-        </ProtectedRoute>
-      } />
-      <Route path="/purchases" element={
-        <ProtectedRoute>
-          <Purchases />
-        </ProtectedRoute>
-      } />
-      <Route path="/deposit" element={
-        <ProtectedRoute>
-          <Deposit />
-        </ProtectedRoute>
-      } />
-      <Route path="/withdraw" element={
-        <ProtectedRoute>
-          <Withdraw />
-        </ProtectedRoute>
-      } />
-      <Route path="/notifications" element={
-        <ProtectedRoute>
-          <Notifications />
-        </ProtectedRoute>
-      } />
-      <Route path="/settings" element={
-        <ProtectedRoute>
-          <Settings />
-        </ProtectedRoute>
-      } />
-      <Route path="/logout" element={
-        <ProtectedRoute>
-          <Logout />
-        </ProtectedRoute>
-      } />
+      <Route
+        path="/transfer"
+        element={
+          <ProtectedRoute>
+            <Transfer />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/transactions"
+        element={
+          <ProtectedRoute>
+            <Transactions />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/forgot-password"
+        element={
+          <ProtectedRoute>
+            <ForgotPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/change-password"
+        element={
+          <ProtectedRoute>
+            <ChangePassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/purchases"
+        element={
+          <ProtectedRoute>
+            <Purchases />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/deposit"
+        element={
+          <ProtectedRoute>
+            <Deposit />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/withdraw"
+        element={
+          <ProtectedRoute>
+            <Withdraw />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/notifications"
+        element={
+          <ProtectedRoute>
+            <Notifications />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <Settings />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/logout"
+        element={
+          <ProtectedRoute>
+            <Logout />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
